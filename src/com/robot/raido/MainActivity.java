bb71bf0524f9d4abc79e35f66741eca0bf8a8c7b
tonapa <tonapa@Gmail.com>
2013-02-26 18:33:04 +0400

    button listeners

--- src/com/robot/raido/MainActivity.java
+++ src/com/robot/raido/MainActivity.java
@@ -9,23 +9,53 @@ public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final BTConnector connector = new BTConnector(this);
+        if (connector.init())
+            connector.connect();
+        else
+            Toast.makeText(this, "Unable to find the other party", 
+                           Toast.LENGTH_LONG).show();
+        
 		Button b = (Button)findViewById(R.id.btn_f);
 		
+        // setup button listeners
 		b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				
+				connector.send((byte)119);
 			}
-		});
+		});       
 		
-		BTConnector connector = new BTConnector(this);
-		if (connector.init())
-			connector.connect();
-		else
-			Toast.makeText(this, "Unable to find the other party", 
-							Toast.LENGTH_LONG).show();
+        b = (Button)findViewById(R.id.btn_b);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                connector.send((byte)115);
+            }
+        });
+            
+        b = (Button)findViewById(R.id.btn_l);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                connector.send((byte)97);                
+            }
+        });
+            
+        b = (Button)findViewById(R.id.btn_r);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                connector.send((byte)100);                
+            }
+        });
+        
+        b = (Button)findViewById(R.id.btn_s);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                connector.send((byte)32);
+            }
+        });
+        
     }
 }
