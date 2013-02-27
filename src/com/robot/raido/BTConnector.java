a3a754a316d6b19d1e2f15066b659a33216e924d
Andrew Lyamov <alyamov@delta.russia.sun.com>
2013-02-27 13:47:20 +0400

    +DeviceReader class

--- src/com/robot/raido/BTConnector.java
+++ src/com/robot/raido/BTConnector.java
@@ -68,4 +68,62 @@ public class BTConnector {
 	private void fake(String s) {
 		handler.sendMessage(Message.obtain(handler, 0, s));
 	}
+
+    private class DeviceReader extends Thread {
+        private static String TAG1 = "DeviceReader";
+        private boolean reading = false;
+        private BufferedInputStream in = null;
+        
+        private int available = 0;
+
+        synchronized public void run() {
+            try {
+                in = new BufferedInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                Toast.makeToast(context, "Unable to read from device", 
+                      Toast.LENGTH_LONG);
+                Log.e(TAG1, "Unable to get InputStream");
+                return;
+            }
+            if (in == null) {
+                Log.e(TAG1, "InputStream == null");
+                return;
+            }
+
+            reading = true;
+            while(reading) {
+                int available = in.available();
+                    
+                try {
+                    notify();
+                    wait(300);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+            }
+            Log.w(TAG1, "ran out");
+        }
+
+        synchronized public void stop() {
+            reading = false;
+        }
+
+        synchronized public int available() {
+            return available;
+        }
+
+        synchronized public byte[] read() {
+            int a = in.available();
+            byte[] buf = new byte[a];
+            
+            try {
+                in.read(buf, 0, a);
+            } catch (IOException e) {
+                Log.e(TAG1, "IOException while reading from stream");
+                return null;
+            }
+            notify();
+            return buf;
+        }
+    }
 }
